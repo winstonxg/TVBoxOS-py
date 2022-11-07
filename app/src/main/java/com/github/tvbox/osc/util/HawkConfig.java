1f0f0672647ef108f78128e6c66996d1e63a5016
watson1982 <112817572+watson1982@users.noreply.github.com>
2022-11-07 20:37:21 +0800

    Add files via upload

--- app/src/main/java/com/github/tvbox/osc/util/HawkConfig.java
+++ app/src/main/java/com/github/tvbox/osc/util/HawkConfig.java
@@ -1,5 +1,7 @@
 package com.github.tvbox.osc.util;
 
+import com.orhanobut.hawk.Hawk;
+
 /**
  * @author pj567
  * @date :2020/12/23
@@ -7,10 +9,8 @@ package com.github.tvbox.osc.util;
  */
 public class HawkConfig {
     public static final String API_URL = "api_url";
-    public static final String EPG_URL = "epg_url";
     public static final String SHOW_PREVIEW = "show_preview";
     public static final String API_HISTORY = "api_history";
-    public static final String EPG_HISTORY = "epg_history";
     public static final String HOME_API = "home_api";
     public static final String DEFAULT_PARSE = "parse_default";
     public static final String DEBUG_OPEN = "debug_open";
@@ -22,19 +22,22 @@ public class HawkConfig {
     public static final String PLAY_TIME_STEP = "play_time_step"; //0 texture 2
     public static final String DOH_URL = "doh_url";
     public static final String HOME_REC = "home_rec"; // 0 豆瓣热播 1 数据源推荐 2 历史
-    public static final String HISTORY_NUM = "history_num";
+    public static final String HOME_NUM = "home_num"; // No. of History
     public static final String SEARCH_VIEW = "search_view"; // 0 列表 1 缩略图
+    public static final String STORAGE_DRIVE_SORT = "storage_drive_sort";
     public static final String LIVE_CHANNEL = "last_live_channel_name";
     public static final String LIVE_CHANNEL_REVERSE = "live_channel_reverse";
     public static final String LIVE_CROSS_GROUP = "live_cross_group";
     public static final String LIVE_CONNECT_TIMEOUT = "live_connect_timeout";
     public static final String LIVE_SHOW_NET_SPEED = "live_show_net_speed";
     public static final String LIVE_SHOW_TIME = "live_show_time";
-    public static final String FAST_SEARCH_MODE = "fast_search_mode";
+    public static final String HOME_SHOW_SOURCE = "show_source";
+    public static final String HOME_LOCALE = "language"; // 0 中文 1 英文
+    public static final String PIC_IN_PIC = "pic_in_pic"; // true = on, false = off
     public static final String SUBTITLE_TEXT_SIZE = "subtitle_text_size";
     public static final String SUBTITLE_TIME_DELAY = "subtitle_time_delay";
-    public static final String SOURCES_FOR_SEARCH = "checked_sources_for_search";
-    public static final String HOME_REC_STYLE = "home_rec_style";
-    public static final String NOW_DATE = "now_date"; //当前日期
-    public static final String REMOTE_TVBOX = "remote_tvbox_host";
+
+    public static boolean isDebug(){
+        return Hawk.get(DEBUG_OPEN, false);
+    }
 }
